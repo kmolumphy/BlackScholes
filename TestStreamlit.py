27639d1c72c7089ac9de4fae17a162aafbf3aaca
Kevin <kmolumphy@gmail.com>
2025-11-03 13:31:39 -0800

    Added heatmap calculation and testing funcs

--- TestStreamlit.py
+++ TestStreamlit.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
 
 # Define Black Scholes inputs as globals for ease of access
 global_stock_price = 100
 global_strike_price = 110
-global_time_to_expiry = 0.1
+global_time_to_expiry = 1.1
 global_volatility = 0.2
-global_interest_rate = 110
+global_interest_rate = 0.1
+
+# Define Heat Map Variables
+global_map_dimension = 10
+global_min_spot_price = 90 #global_stock_price * 0.9
+global_max_spot_price = 110 #global_stock_price * 1.1
+global_min_volatility = 0.01
+global_max_volatility = 1
 
 
 def main():
@@ -18,35 +28,81 @@ def main():
 def CreateStreamLitInterface():
     st.write("Black Scholes")
 
-    global_stock_price = st.number_input("Stock Price")
-    global_strike_price = st.number_input("Strike Price")
-    global_time_to_expiry = st.number_input("Time to Expiration")
-    global_volatility = st.number_input("Volatility")
-    global_interest_rate = st.number_input("Risk-Free Interest Rate ")
+    # Create Input Var Widgets
+    global_stock_price = st.number_input("Stock Price", placeholder=globals()['global_stock_price'])
+    global_strike_price = st.number_input("Strike Price", placeholder=globals()['global_strike_price'])
+    global_time_to_expiry = st.number_input("Time to Expiration", placeholder=globals()['global_time_to_expiry'])
+    global_volatility = st.number_input("Volatility", placeholder=globals()['global_volatility'])
+    global_interest_rate = st.number_input("Risk-Free Interest Rate ", placeholder=globals()['global_interest_rate'])
+
+    # Create heat maps based on inputs
+    CreateHeatMap("PutHeatMap", CreatePutDataframe())
+    CreateHeatMap("Call Heat Map", CreateCallDataframe())
+
+def CreatePutHeatMap():
+    fig, ax = plt.subplots() 
+
+def CreateHeatMap(title, dataframe):
+    print(dataframe)
+
+    data = np.random.rand(10, 10)
+    df = pd.DataFrame(data, columns=[f'Col {i}' for i in range(10)])
+    # Create a heatmap using Seaborn
+    plt.figure(figsize=(global_map_dimension, global_map_dimension))
+    sns.heatmap(df, annot=True, cmap='coolwarm')
+
+    # Show the plot in Streamlit
+    st.pyplot(plt)
 
 
-def CreateStockHeatMap():
-    fig, ax = plt.subplots()  ## <- Create matplotlib Figure & Axes
+def CreateCallDataframe():
+    data = []
+    for row in range(global_map_dimension):
+        data.append(CreateCallRow(row))    
+    df = pd.DataFrame(data)
 
-    july.heatmap(
-        dates=df.Dates,
-        data=df.Pages,
-        cmap='github',
-        month_grid=True,
-        horizontal=True,
-        value_label=True,
-        date_label=False,
-        weekday_label=True,
-        month_label=True,
-        year_label=True,
-        colorbar=True,
-        fontfamily="monospace",
-        fontsize=12,
-        title="Daily Pages Read",
-        ax=ax   ## <- Tell July to put the heatmap in that Axes
-    )
+def CreateCallRow(CurrentRow):
+    row = []
+    # Map Current row to volatility
+    CurrentVolatility = GetVolatility(CurrentRow)
+    for CurrentColumn in range(global_map_dimension):
+        CurrentSpotPrice = GetCurrentSpotPrice(CurrentColumn)
+        row.append(CalculateCall(CurrentVolatility, CurrentSpotPrice))
+    return row
 
-    st.pyploy(fig)
+def CreatePutDataframe():
+    data = []
+    for row in range(global_map_dimension):
+        data.append(CreatePutRow(row))
+    df = pd.DataFrame(data)
+
+def CreatePutRow(CurrentRow):
+    row = []
+    # Map Current row to volatility
+    CurrentVolatility = GetVolatility(CurrentRow)
+    for CurrentColumn in range(global_map_dimension):
+        CurrentSpotPrice = GetCurrentSpotPrice(CurrentColumn)
+        row.append(CalculatePut(CurrentVolatility, CurrentSpotPrice))
+    print(row)
+    return row
+
+
+def CalculateCall(Volatility, SpotPrice):
+    # Temp return val to test
+    return Volatility * SpotPrice
+
+def CalculatePut(Volatility, SpotPrice):
+    # Temp return val to test
+    return Volatility * SpotPrice
+
+def GetVolatility(RowNum):
+    CurrentVolatility = (RowNum / (global_map_dimension - 1)) * ((global_max_volatility - global_min_volatility) + global_min_volatility)
+    return CurrentVolatility
+
+def GetCurrentSpotPrice(ColumnNum):
+    CurrentVolatility = (ColumnNum / (global_map_dimension - 1)) * ((global_max_spot_price - global_min_spot_price) + global_min_spot_price)
+    return CurrentVolatility
+
 
 if __name__ == "__main__":
     main()
